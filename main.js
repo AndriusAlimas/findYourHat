809a0fa67cb57fa454462dbcdac4cbeba4c17d99
Andrius <alimasWebDeveloper@gmail.com>
2021-07-10 22:36:34 +0100

    Update main.js
    Update main.js created constructor for 2d field that accepting an array. Starting creating method print that will print out a string representation of the board.

--- main.js
+++ main.js
@@ -5,4 +5,23 @@ const hole = "O";
 const fieldCharacter = "░";
 const pathCharacter = "*";
 
-class Field {}
+class Field {
+  // constructor
+  constructor(field2D) {
+    this.field2D = field2D;
+  }
+
+  // print out a string representation of the board
+  print() {
+    for (let i = 0; i < this.field2D.length; i++) {
+      console.log(this.field2D[i]);
+    }
+  }
+}
+
+const myField = new Field([
+  ["*", "░", "O"],
+  ["░", "O", "░"],
+  ["░", "^", "░"],
+]);
+myField.print();
