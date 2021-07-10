81ddfa309689fce5f57c440f6596bdfc7410b27b
Andrius <alimasWebDeveloper@gmail.com>
2021-07-10 23:00:43 +0100

    Update main.js
    Created function playGame() there will be start row and column position at 0. In this method create 2dField playable grid. And in this function will be asked user input where to move.
    
    Created updateFieldArray() method where we always updating our map when player will move, we have to re-draw

--- main.js
+++ main.js
@@ -16,15 +16,45 @@ class Field {
     let result = "";
     for (let i = 0; i < this.field2D.length; i++) {
       result += this.field2D[i].join(" ");
-      result += "\n";
+      result += "\n"; // next line
     }
     console.log(result);
   }
+
+  // this method update map when you move
+  updateFieldArray(row, col) {
+    try {
+      this.field2D[row][col] = "*"; // this will be rendered your position (player)
+    } catch (e) {
+      // other exception catch like array out of bound exception
+      console.log("sorry, u can't move to this place its wall!");
+    }
+  }
 }
 
-const myField = new Field([
-  ["*", "░", "O"],
-  ["░", "O", "░"],
-  ["░", "^", "░"],
-]);
-myField.print();
+function playGame() {
+  // user starting position
+  let rowIndex = 0;
+  let columnIndex = 0;
+
+  // print current field
+  const myField = new Field([
+    ["*", "░", "O"],
+    ["░", "O", "░"],
+    ["░", "^", "░"],
+  ]);
+  myField.print();
+  // get user input
+  let userInput = prompt("Which direction? : ");
+
+  // add or subtract columnIndex if user input is l(left) or r(right)
+  columnIndex = userInput === "r" ? columnIndex++ : columnIndex--;
+  // add or subtract rowIndex if user input is u(up) or d(down)
+  rowIndex = userInput === "d" ? rowIndex++ : rowIndex--;
+
+  // update field array
+
+  // print out current field array
+}
+
+playGame();
